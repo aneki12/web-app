22580a4b61d25c74316430be1d5d7254d235e49a
Atman Das <atmandas69@gmail.com>
2022-01-24 16:44:45 +0530

    changes made to forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,6 +1,6 @@
 const request = require("request")
 const forecast =(longitude,latitude,callback)=>{
-    const url = "http://api.weatherstack.com/current?access_key=755299d5a6b134b3224f726db36622e3&query="+longitude+","+latitude+"&units=f"
+    const url = "http://api.weatherstack.com/current?access_key=755299d5a6b134b3224f726db36622e3&query="+longitude+","+latitude
     request({url,json:true},(error,{body}={})=>{
         if(error)
         {
@@ -10,7 +10,7 @@ const forecast =(longitude,latitude,callback)=>{
             callback("wrong input")
         }
         else{
-            callback("",body.current.weather_descriptions[0]+"it is currently "+body.current.temperature+" degreed out.It feels like " +body.current.feelslike+" degrees out here .")
+            callback("",body.current.weather_descriptions[0]+" it is currently "+body.current.temperature+" degreed out.It feels like " +body.current.feelslike+" degrees out here . The humidity is "+body.current.humidity+"%")
         }
        //   console.log(response.body.current)
        
